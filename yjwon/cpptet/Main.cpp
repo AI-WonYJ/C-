a6f21d20c9ee8f7f941b559d324c40cb6c9cef23
WonYoungJin <yjason7587@naver.com>
2023-04-13 23:12:58 +0900

    테트리스 과제 1

---
+++ yjwon/cpptet/Main.cpp
@@ -0,0 +1,298 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <stdio.h>
+#include <termios.h>
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+#include "colors.h"
+#include "Matrix.h"
+
+using namespace std;
+
+
+/**************************************************************/
+/**************** Linux System Functions **********************/
+/**************************************************************/
+
+char saved_key = 0;
+int tty_raw(int fd);	/* put terminal into a raw mode */
+int tty_reset(int fd);	/* restore terminal's mode */
+  
+/* Read 1 character - echo defines echo mode */
+char getch() {
+  char ch;
+  int n;
+  while (1) {
+    tty_raw(0);
+    n = read(0, &ch, 1);
+    tty_reset(0);
+    if (n > 0)
+      break;
+    else if (n < 0) {
+      if (errno == EINTR) {
+        if (saved_key != 0) {
+          ch = saved_key;
+          saved_key = 0;
+          break;
+        }
+      }
+    }
+  }
+  return ch;
+}
+
+void sigint_handler(int signo) {
+  // cout << "SIGINT received!" << endl;
+  // do nothing;
+}
+
+void sigalrm_handler(int signo) {
+  alarm(1);
+  saved_key = 's';
+}
+
+void registerInterrupt() {
+  struct sigaction act, oact;
+  act.sa_handler = sigint_handler;
+  sigemptyset(&act.sa_mask);
+#ifdef SA_INTERRUPT
+  act.sa_flags = SA_INTERRUPT;
+#else
+  act.sa_flags = 0;
+#endif
+  if (sigaction(SIGINT, &act, &oact) < 0) {
+    cerr << "sigaction error" << endl;
+    exit(1);
+  }
+}
+
+void registerAlarm() {
+  struct sigaction act, oact;
+  act.sa_handler = sigalrm_handler;
+  sigemptyset(&act.sa_mask);
+#ifdef SA_INTERRUPT
+  act.sa_flags = SA_INTERRUPT;
+#else
+  act.sa_flags = 0;
+#endif
+  if (sigaction(SIGALRM, &act, &oact) < 0) {
+    cerr << "sigaction error" << endl;
+    exit(1);
+  }
+  alarm(1);
+}
+
+/**************************************************************/
+/**************** Tetris Blocks Definitions *******************/
+/**************************************************************/
+#define MAX_BLK_TYPES 7
+#define MAX_BLK_DEGREES 4
+
+//7가지 블록
+// -1: 배열의 끝
+int T0D0[] = { 1, 1, 1, 1, -1 }; //2x2
+int T0D1[] = { 1, 1, 1, 1, -1 };
+int T0D2[] = { 1, 1, 1, 1, -1 };
+int T0D3[] = { 1, 1, 1, 1, -1 };
+
+int T1D0[] = { 0, 1, 0, 1, 1, 1, 0, 0, 0, -1 }; //3x3
+int T1D1[] = { 0, 1, 0, 0, 1, 1, 0, 1, 0, -1 };
+int T1D2[] = { 0, 0, 0, 1, 1, 1, 0, 1, 0, -1 };
+int T1D3[] = { 0, 1, 0, 1, 1, 0, 0, 1, 0, -1 };
+
+int T2D0[] = { 1, 0, 0, 1, 1, 1, 0, 0, 0, -1 }; //3x3
+int T2D1[] = { 0, 1, 1, 0, 1, 0, 0, 1, 0, -1 };
+int T2D2[] = { 0, 0, 0, 1, 1, 1, 0, 0, 1, -1 };
+int T2D3[] = { 0, 1, 0, 0, 1, 0, 1, 1, 0, -1 };
+
+int T3D0[] = { 0, 0, 1, 1, 1, 1, 0, 0, 0, -1 }; //3x3
+int T3D1[] = { 0, 1, 0, 0, 1, 0, 0, 1, 1, -1 };
+int T3D2[] = { 0, 0, 0, 1, 1, 1, 1, 0, 0, -1 };
+int T3D3[] = { 1, 1, 0, 0, 1, 0, 0, 1, 0, -1 };
+
+int T4D0[] = { 0, 1, 0, 1, 1, 0, 1, 0, 0, -1 }; //3x3
+int T4D1[] = { 1, 1, 0, 0, 1, 1, 0, 0, 0, -1 };
+int T4D2[] = { 0, 1, 0, 1, 1, 0, 1, 0, 0, -1 };
+int T4D3[] = { 1, 1, 0, 0, 1, 1, 0, 0, 0, -1 };
+
+int T5D0[] = { 0, 1, 0, 0, 1, 1, 0, 0, 1, -1 }; //3x3
+int T5D1[] = { 0, 0, 0, 0, 1, 1, 1, 1, 0, -1 };
+int T5D2[] = { 0, 1, 0, 0, 1, 1, 0, 0, 1, -1 };
+int T5D3[] = { 0, 0, 0, 0, 1, 1, 1, 1, 0, -1 };
+
+int T6D0[] = { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, -1 }; //4x4
+int T6D1[] = { 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, -1 };
+int T6D2[] = { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, -1 };
+int T6D3[] = { 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, -1 };
+  
+int *setOfBlockArrays[] = {  // 28개의 도형의 주소를 모아놓음
+  T0D0, T0D1, T0D2, T0D3,
+  T1D0, T1D1, T1D2, T1D3,
+  T2D0, T2D1, T2D2, T2D3,
+  T3D0, T3D1, T3D2, T3D3,
+  T4D0, T4D1, T4D2, T4D3,
+  T5D0, T5D1, T5D2, T5D3,
+  T6D0, T6D1, T6D2, T6D3,
+};
+
+void drawScreen(Matrix *screen, int wall_depth)
+{
+  int dy = screen->get_dy();
+  int dx = screen->get_dx();
+  int dw = wall_depth;
+  int **array = screen->get_array();
+
+  for (int y = 0; y < dy - dw + 1; y++) {
+    for (int x = dw - 1; x < dx - dw + 1; x++) {
+      if (array[y][x] == 0)
+	      cout << "□ ";
+      else if (array[y][x] == 1)
+	      cout << "■ ";
+      else if (array[y][x] == 10)
+	      cout << "◈ ";
+      else if (array[y][x] == 20)
+	      cout << "★ ";
+      else if (array[y][x] == 30)
+	      cout << "● ";
+      else if (array[y][x] == 40)
+	      cout << "◆ ";
+      else if (array[y][x] == 50)
+	      cout << "▲ ";
+      else if (array[y][x] == 60)
+	      cout << "♣ ";
+      else if (array[y][x] == 70)
+	      cout << "♥ ";
+      else
+	      cout << "X ";
+    }
+    cout << endl;
+  }
+}
+  
+/**************************************************************/
+/******************** Tetris Main Loop ************************/
+/**************************************************************/
+
+#define SCREEN_DY  10
+#define SCREEN_DX  10
+#define SCREEN_DW  3
+
+#define ARRAY_DY (SCREEN_DY + SCREEN_DW)
+#define ARRAY_DX (SCREEN_DX + 2*SCREEN_DW)
+
+int arrayScreen[ARRAY_DY][ARRAY_DX] = {  // 충돌을 막아주기 위해 벽 두께를 3으로 만들어줌
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },  
+  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },  
+  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+};
+
+int arrayBlk[3][3] = {
+  { 0, 1, 0 },
+  { 1, 1, 1 },
+  { 0, 0, 0 },
+};
+
+int main(int argc, char *argv[]) {
+  char key;
+  int top = 0, left = 4;
+
+  int random = (rand() % 6 + 1);
+
+  Matrix *iScreen = new Matrix((int *) arrayScreen, ARRAY_DY, ARRAY_DX);  // (int*): 2차원배열 1차원으로 강제 입력, 이유: 몇차원일지 모르기 때문에  1차원으로 형변환 > 생성자를 한 번만 쓰면 됨
+  Matrix *currBlk = new Matrix((int *) arrayBlk,    3, 3);
+  Matrix *tempBlk = iScreen->clip(top, left, top + currBlk->get_dy(), left + currBlk->get_dx());
+  // tempBlk = tempBlk->add(currBlk);  // 유실 발생, pointer로 작성해야함
+  Matrix *tempBlk2 = tempBlk->add(currBlk);
+  delete tempBlk;
+
+  Matrix *oScreen = new Matrix(iScreen);
+  oScreen->paste(tempBlk2, top, left);
+  delete tempBlk2;
+  drawScreen(oScreen, SCREEN_DW);
+  delete oScreen;
+  while ((key = getch()) != 'q') {  //getch: 터미널에서 사용자 입력값을 받아옴
+    switch (key) {
+      case 'a': left--; break;
+      case 'd': left++; break;
+      case 's': top++; break;
+      case 'w': top--; break;
+      case ' ': {
+        while (true) {  // " " 눌렸을 때 최하단으로 블럭 이동
+          top++;
+          tempBlk = iScreen->clip(top, left, top + currBlk->get_dy(), left + currBlk->get_dx());
+          tempBlk2 = tempBlk->add(currBlk);
+          delete tempBlk;
+          if (tempBlk2->anyGreaterThan(1)) {
+            top--;
+            break;
+          }
+        }
+      }; break;
+      default: cout << "wrong key input" << endl;
+    }
+      tempBlk = iScreen->clip(top, left, top + currBlk->get_dy(), left + currBlk->get_dx());
+      tempBlk2 = tempBlk->add(currBlk);
+      delete tempBlk;
+
+      if (tempBlk2->anyGreaterThan(1)) {
+        delete tempBlk2;
+        switch (key) {
+          case 'a': left++; break;
+          case 'd': left--; break;
+          case 's': top--; break;
+          case 'w': top++; break;
+          case ' ': break;
+          default: cout << "wrong key input" << endl;
+        }
+        // cout << "Warning" << endl;
+        tempBlk = iScreen->clip(top, left, top + currBlk->get_dy(), left + currBlk->get_dx());
+        tempBlk2 = tempBlk->add(currBlk);
+        delete tempBlk;
+      }
+
+      oScreen = new Matrix(iScreen);
+      oScreen->paste(tempBlk2, top, left);
+      delete tempBlk2;
+      drawScreen(oScreen, SCREEN_DW);
+      delete oScreen;
+
+      
+      top++;
+      tempBlk = iScreen->clip(top, left, top + currBlk->get_dy(), left + currBlk->get_dx());
+      tempBlk2 = tempBlk->add(currBlk);
+      delete tempBlk;
+      if (tempBlk2->anyGreaterThan(1)) {
+        iScreen->paste(oScreen,top, left);
+        drawScreen(oScreen, SCREEN_DW);
+        delete oScreen;
+      }
+      delete tempBlk2;
+      top--;
+  }
+
+  delete iScreen;
+  delete currBlk;
+  // delete tempBlk;
+  // delete oScreen;
+  // delete tempBlk2;  //add
+
+  cout << "(nAlloc, nFree) = (" << Matrix::get_nAlloc() << ',' << Matrix::get_nFree() << ")" << endl;  
+  cout << "Program terminated!" << endl;
+
+  return 0;
+}
